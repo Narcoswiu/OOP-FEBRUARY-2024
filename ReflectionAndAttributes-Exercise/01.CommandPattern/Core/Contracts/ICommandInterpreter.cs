b80b572cb745f3487b751d4afbd4a4deb04c8e4e
Николай Тодоров <narcoswiu324@gmail.com>
2024-03-13 19:09:08 +0200

    Add new lecture for ReflectionAndAttributes

---
+++ ReflectionAndAttributes-Exercise/01.CommandPattern/Core/Contracts/ICommandInterpreter.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace CommandPattern.Core.Contracts
+{
+    public interface ICommandInterpreter
+    {
+        string Read(string args);
+    }
+}
---
+++ ReflectionAndAttributes-Exercise/01.CommandPattern/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using CommandPattern.Core;
+using CommandPattern.Core.Commands;
+using CommandPattern.Core.Contracts;
+using System.Data;
+
+namespace CommandPattern
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            ICommandInterpreter command = new CommandInterpreter();
+            IEngine engine = new Engine(command);
+            engine.Run();
+
+        }
+    }
+}
